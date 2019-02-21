885aaf3f7eb97f2f43a1a7c739ff48ff7f08182a
LeoAttn <leo.massonnat@hotmail.fr>
2019-02-21 22:36:59 +0100

    add unit tests

--- src/test/book.js
+++ src/test/book.js
@@ -20,7 +20,7 @@ describe('Test intégration (Empty database)', () => {
     books: []
   }
   beforeEach(() => {
-    resetDatabase(path.join(__dirname, '../data/books.json'), emptyBooks)
+    resetDatabase(path.join(__dirname, '../main/data/books.json'), emptyBooks)
   })
 
   it('should return empty database', done => {
@@ -70,7 +70,7 @@ describe('Test intégration (Mocked database)', () => {
     ]
   }
   beforeEach(() => {
-    resetDatabase(path.join(__dirname, '../data/books.json'), oneBooks)
+    resetDatabase(path.join(__dirname, '../main/data/books.json'), oneBooks)
   })
 
   it('should update a book', done => {
@@ -120,6 +120,193 @@ describe('Test intégration (Mocked database)', () => {
         expect(res.body.book.years).to.equal(oneBooks.books[0].years)
         expect(res.body.book.pages).to.be.a('number')
         expect(res.body.book.pages).to.equal(oneBooks.books[0].pages)
+        done()
+      })
+  })
+})
+
+describe('Test unitaire (simulation de réponse ok)', () => {
+  // beforeEach(()=>{
+  //   nock.disableNetConnect();
+  //   nock.enableNetConnect(/^(127\.0\.0\.1|localhost)/);
+  // });
+  afterEach(() => {
+    nock.cleanAll()
+    // nock.restore()
+  })
+
+  it('should get all books', done => {
+    nock('http://localhost:8080')
+      .get('/book')
+      .reply(200, {
+        books: [{
+          id: '0db0b43e-dddb-47ad-9b4a-e5fe9ec7c2a9',
+          title: 'Coco raconte Channel 33',
+          years: 1990,
+          pages: 400
+        }]
+      })
+
+    chai
+      .request('http://localhost:8080')
+      .get('/book')
+      .end((err, res) => {
+        if (err) console.log(err)
+        console.log(res.body)
+        expect(res).to.have.status(200)
+        expect(res.body).to.be.a('object')
+        expect(res.body.books).to.be.a('array')
+        done()
+      })
+  })
+
+  it('should add a book', done => {
+    nock('http://localhost:8080')
+      .post('/book')
+      .reply(200, {
+        message: 'book successfully added'
+      })
+
+    chai
+      .request('http://localhost:8080')
+      .post('/book')
+      .end((err, res) => {
+        if (err) console.log(err)
+        console.log(res.body)
+        expect(res).to.have.status(200)
+        expect(res.body).to.be.a('object')
+        expect(res.body.message).to.equal('book successfully added')
+        done()
+      })
+  })
+
+  it('should update a book', done => {
+    nock('http://localhost:8080')
+      .put('/book')
+      .reply(200, {
+        message: 'book successfully updated'
+      })
+
+    chai
+      .request('http://localhost:8080')
+      .put('/book')
+      .end((err, res) => {
+        if (err) console.log(err)
+        console.log(res.body)
+        expect(res).to.have.status(200)
+        expect(res.body).to.be.a('object')
+        expect(res.body.message).to.equal('book successfully updated')
+        done()
+      })
+  })
+
+  it('should delete a book', done => {
+    nock('http://localhost:8080')
+      .delete('/book')
+      .reply(200, {
+        message: 'book successfully deleted'
+      })
+
+    chai
+      .request('http://localhost:8080')
+      .delete('/book')
+      .end((err, res) => {
+        if (err) console.log(err)
+        console.log(res.body)
+        expect(res).to.have.status(200)
+        expect(res.body).to.be.a('object')
+        expect(res.body.message).to.equal('book successfully deleted')
+        done()
+      })
+  })
+})
+
+describe('Test unitaire (simulation de mauvaise réponse)', () => {
+  // beforeEach(()=>{
+  //   nock.disableNetConnect();
+  //   nock.enableNetConnect(/^(127\.0\.0\.1|localhost)/);
+  // });
+  afterEach(() => {
+    nock.cleanAll()
+    // nock.restore()
+  })
+
+  it('should have an error to get all books', done => {
+    nock('http://localhost:8080')
+      .get('/book')
+      .reply(400, {
+        message: 'error fetching books'
+      })
+
+    chai
+      .request('http://localhost:8080')
+      .get('/book')
+      .end((err, res) => {
+        if (err) console.log(err)
+        console.log(res.body)
+        expect(res).to.have.status(400)
+        expect(res.body).to.be.a('object')
+        expect(res.body.message).to.equal('error fetching books')
+        done()
+      })
+  })
+
+  it('should have an error to add a book', done => {
+    nock('http://localhost:8080')
+      .post('/book')
+      .reply(400, {
+        message: 'error adding the book'
+      })
+
+    chai
+      .request('http://localhost:8080')
+      .post('/book')
+      .end((err, res) => {
+        if (err) console.log(err)
+        console.log(res.body)
+        expect(res).to.have.status(400)
+        expect(res.body).to.be.a('object')
+        expect(res.body.message).to.equal('error adding the book')
+        done()
+      })
+  })
+
+  it('should have an error to update a book', done => {
+    nock('http://localhost:8080')
+      .put('/book')
+      .reply(400, {
+        message: 'error updating the book'
+      })
+
+    chai
+      .request('http://localhost:8080')
+      .put('/book')
+      .end((err, res) => {
+        if (err) console.log(err)
+        console.log(res.body)
+        expect(res).to.have.status(400)
+        expect(res.body).to.be.a('object')
+        expect(res.body.message).to.equal('error updating the book')
+        done()
+      })
+  })
+
+  it('should have an error to delete a book', done => {
+    nock('http://localhost:8080')
+      .delete('/book')
+      .reply(400, {
+        message: 'error deleting the book'
+      })
+
+    chai
+      .request('http://localhost:8080')
+      .delete('/book')
+      .end((err, res) => {
+        if (err) console.log(err)
+        console.log(res.body)
+        expect(res).to.have.status(400)
+        expect(res.body).to.be.a('object')
+        expect(res.body.message).to.equal('error deleting the book')
         done()
       })
   })
